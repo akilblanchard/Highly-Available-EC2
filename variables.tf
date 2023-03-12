4fedf01fd58f23f0ece626784afad93a496a3644
AkilBlanchard <akilblanchard@github.com>
2023-03-12 12:44:08 -0500

    added variables for subnet config

---
+++ variables.tf
@@ -0,0 +1,31 @@
+
+variable "main_vpc" {
+  description = "VPC for ec2 instances"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "ami_id"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type to create an instance"
+  type        = string
+}
+
+
+variable "ssh_private_key" {
+  description = "pem file of Keypair we used to login to EC2 instances"
+  type        = string
+}
+
+variable "subnets_cidr" {
+  description = "cidr values for 3 public subnets"
+  type = list
+}
+
+variable "azs" {
+  description = "Availability Zones vpc will span"
+  type = list
+}
--- variables.tfvars
+++ variables.tfvars
@@ -1,5 +1,5 @@
 #vpc
-main = "10.0.0.0/16"
+main_vpc = "10.0.0.0/16"
 
 #ami for ec2 instances
 ami_id = "ami-0557a15b87f6559cf"
@@ -8,3 +8,9 @@ ami_id = "ami-0557a15b87f6559cf"
 instance_type = "t2.micro"
 
 ssh_private_key = "./key-pair.pem"
+
+#CIDRs for subnets
+subnets_cidr = ["10.0.1.0/24, 10.0.2.0/24, 10.0.3.0/24"]
+
+#Availability Zones
+azs = ["us-east-1a", "us-east-1b", "us-east-1c" ]
