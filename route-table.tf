9cba86ace8b6b18dd46e48d99fe054ffbfb0ef20
AkilBlanchard <akilblanchard@github.com>
2023-03-12 13:17:58 -0500

    Configured route tabel and route table association

---
+++ route-table.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public_route" {
+    vpc_id = aws_vpc.main_vpc.id
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gatyeway_id = aws_internet_gateway.main-gateway.id
+    }
+  
+}
+
+#Route Table Association with public subnets
+
+resource "aws_route_table_association" "a" {
+    count = length(var.subnets_cidr)
+    subnet_id = element(aws_subnet.public.*.id,count.index)
+    route_table_id = aws_route_table.public_route.id
+  
+}
