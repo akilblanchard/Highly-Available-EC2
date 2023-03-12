40d5847b3abef1362375a5804787817065e0a440
AkilBlanchard <akilblanchard@github.com>
2023-03-12 14:37:29 -0500

    added down scale policy and alarm

--- auto-scaling-group.tf
+++ auto-scaling-group.tf
@@ -47,3 +47,32 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
   actions_enabled = true
   alarm_actions   = [aws_autoscaling_policy.scale_up.arn]
 }
+
+#Scale down policy
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "asg-scale-down"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "-1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+#Scale down cloud watch alarm
+resource "aws_cloudwatch_metric_alarm" "scale-down-alarm" {
+  alarm_name          = "asg-scale-down-alarm"
+  alarm_description   = "asg-scale-down-cpu-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
+}
