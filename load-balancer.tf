858d916465a6a7f2044ba0b78f75c09c22717ba7
AkilBlanchard <akilblanchard@github.com>
2023-03-12 13:26:43 -0500

    configured load balancer and listener

---
+++ load-balancer.tf
@@ -0,0 +1,24 @@
+#Application Load Balancer
+
+resource "aws_lb" "my_balancer" {
+  name               = "my-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my_security.id]
+  subnets            = element(aws_subnet.public.*.id,count.index)
+}
+
+
+
+#listner ports for load balancer
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.my_balancer.arn
+  port              = "80"
+  protocol          = "Http"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_g.arn
+  }
+}
