fd2eaeda35f90b08f34237dc376083e46b3b2765
AkilBlanchard <akilblanchard@github.com>
2023-03-12 13:29:40 -0500

    configred secuirty group

---
+++ security.tf
@@ -0,0 +1,34 @@
+#Security Group
+
+resource "aws_security_group" "my_security" {
+  name   = "my-secuirty"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "Allow all http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow all http"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "my-security"
+  }
+}
