73ea8c795206468a604a068361ed04bd3e9f9b48
AkilBlanchard <akilblanchard@github.com>
2023-03-12 13:28:20 -0500

    listener ports for load balancer

---
+++ lb-listener.tf
@@ -0,0 +1,12 @@
+#listner ports for load balancer
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.my_balancer.arn
+  port              = "80"
+  protocol          = "Http"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_g.arn
+  }
+}
