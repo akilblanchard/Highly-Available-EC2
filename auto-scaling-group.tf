b7b8ddf91ce4cd6dbbd802b9abb14372c3dba1ce
AkilBlanchard <akilblanchard@github.com>
2023-03-12 19:18:14 -0500

    updated zone identifier

--- auto-scaling-group.tf
+++ auto-scaling-group.tf
@@ -1,14 +1,15 @@
 #Auto Scaling Group
 resource "aws_autoscaling_group" "asg" {
 
-  name              = "my_asg"
+  name              = "asg"
   max_size          = 3
   min_size          = 1
   health_check_type = "ELB"
   desired_capacity  = 2
   target_group_arns = [aws_lb_target_group.target_g.arn]
 
-  vpc_zone_identifier = element(aws_subnet.public.*.id, count.index)
+  vpc_zone_identifier = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
+
 
   launch_template {
     id      = aws_launch_template.launch_template.id
@@ -23,7 +24,7 @@ resource "aws_autoscaling_group" "asg" {
 resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale_up"
   policy_type            = "SimpleScaling"
-  autoscaling_group_name = aws_autoscaling_group.my_asg.name
+  autoscaling_group_name = aws_autoscaling_group.asg.name
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = "1"   #adds one instance
   cooldown               = "300" #cooldown period after scaling
@@ -42,7 +43,7 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
   statistic           = "Average"
   threshold           = "50"
   dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.na
+    "AutoScalingGroupName" = aws_autoscaling_group.asg.name
   }
   actions_enabled = true
   alarm_actions   = [aws_autoscaling_policy.scale_up.arn]
@@ -52,7 +53,7 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
 
 resource "aws_autoscaling_policy" "scale_down" {
   name                   = "asg-scale-down"
-  autoscaling_group_name = aws_autoscaling_group.my_asg.name
+  autoscaling_group_name = aws_autoscaling_group.asg.name
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = "-1"
   cooldown               = "300"
@@ -71,7 +72,7 @@ resource "aws_cloudwatch_metric_alarm" "scale-down-alarm" {
   threshold           = "30"
 
   dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
+    "AutoScalingGroupName" = aws_autoscaling_group.asg.name
   }
   actions_enabled = true
   alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
