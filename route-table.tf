0ceb9d522226ff51bd70d3a6cd15fccdbe121950
AkilBlanchard <akilblanchard@github.com>
2023-03-12 19:19:12 -0500

    updated route table association

--- route-table.tf
+++ route-table.tf
@@ -1,8 +1,8 @@
 resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.main_vpc.id
-  route = {
-    cidr_block  = "0.0.0.0/0"
-    gatyeway_id = aws_internet_gateway.main-gateway.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-gateway.id
   }
 
 }
@@ -10,8 +10,16 @@ resource "aws_route_table" "public_route" {
 #Route Table Association with public subnets
 
 resource "aws_route_table_association" "a" {
-  count          = length(var.subnets_cidr)
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public_route.id
+}
 
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public_route.id
 }
